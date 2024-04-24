805f37a8c11370bf181a83bba6c3495d04426b30
YuserabanoAnsari <yuserabanoansari@dwao.in>
2024-04-24 16:15:43 +0530

    update: updated confg.java

--- app/src/main/java/com/dwao/aliumandroidsdk/Config.java
+++ app/src/main/java/com/dwao/aliumandroidsdk/Config.java
@@ -1,7 +1,6 @@
 package com.dwao.aliumandroidsdk;
 
 public class Config {
-//  public static final String BASE_URL="https://dev-assets.alium.co.in/cmmn/cstjn/cstjn_38.json";
-public static final String BASE_URL="https://assets.alium.co.in/cmmn/cstjn/cstjn_1038.json";
+    public static final String BASE_URL=    "";
 }
 
