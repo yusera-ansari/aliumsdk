3ae9dcab75677b6db98972124cbbf6b6a3c74ae6
YuserabanoAnsari <yuserabanoansari@dwao.in>
2024-06-12 15:33:44 +0530

    fix: fixed bug in recording frequency

--- app/src/main/java/com/dwao/aliumandroidsdk/MainActivity.java
+++ app/src/main/java/com/dwao/aliumandroidsdk/MainActivity.java
@@ -51,6 +51,6 @@ public class MainActivity extends Activity {
             }
         });
 
-        Alium.trigger(this, new SurveyParameters("secondscreen", new HashMap()));
+        Alium.trigger(this, new SurveyParameters("firstscreen", new HashMap()));
     }
 }
