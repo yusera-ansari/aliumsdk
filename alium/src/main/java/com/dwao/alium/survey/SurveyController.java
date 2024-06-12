cee5f64051756b26e96f7f9637fff59c8d17d229
YuserabanoAnsari <yuserabanoansari@dwao.in>
2024-06-12 15:33:44 +0530

    fix: fixed bug in recording frequency

--- alium/src/main/java/com/dwao/alium/survey/SurveyController.java
+++ alium/src/main/java/com/dwao/alium/survey/SurveyController.java
@@ -68,7 +68,7 @@ abstract class SurveyController {
 
      @CallSuper
     protected  void submitSurvey(){
-        recordSurveyTriggerOnPreferences();
+         if(loadableSurveySpecs.surveyFreq.equals("untilresponse"))recordSurveyTriggerOnPreferences();
     };
      private void recordSurveyTriggerOnPreferences(){
           switch (loadableSurveySpecs.surveyFreq) {
