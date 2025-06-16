78aac9839a04ef43fdbb2d6dceda70a849ce8f93
yusera-ansari <yuserabanoansari@dwao.in>
2025-06-16 11:50:00 +0530

    updated ui fixed volley request issue

--- alium/src/main/java/com/dwao/alium/network/VolleyService.java
+++ alium/src/main/java/com/dwao/alium/network/VolleyService.java
@@ -7,6 +7,7 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.dwao.alium.listeners.VolleyResponseListener;
@@ -68,18 +69,18 @@ public class VolleyService {
          queue = Volley.newRequestQueue(context);
     Log.d("fetch-main", url);
        // Request a string response from the provided URL.
-       StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-               new Response.Listener<String>() {
+       JsonObjectRequest stringRequest = new JsonObjectRequest(  url,
+               new Response.Listener<JSONObject>() {
                    @Override
-                   public void onResponse(String response) {
-                       Log.d("response-survey", response);
-                       try {
-                           JSONObject jsonObject=new JSONObject(response);
-                           volleyResponseListener.onResponseReceived(jsonObject);
+                   public void onResponse(JSONObject response) {
+                       Log.d("response-survey", response.toString());
+//                       try {
+//                           JSONObject jsonObject=new JSONObject(response);
+                           volleyResponseListener.onResponseReceived(response);
 
-                       } catch (JSONException e) {
-                        Log.i("surveyResponseExc", e.toString());
-                       }
+//                       } catch (JSONException e) {
+//                        Log.i("surveyResponseExc", e.toString());
+//                       }
 
                    }
                }, new Response.ErrorListener() {
